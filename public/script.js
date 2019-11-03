cbf4ce9a844756f4a20ce764f9a06f5282eacd53
Kevin O'Brien <kob123@hotmail.co.uk>
2019-11-03 15:37:02 +0000

    coded skycons icon

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 const searchElement = document.querySelector('[data-city-search]');
-
 // Make our input field a google search box.
 // Accessed via the google maps API in index.html
 const searchBox = new google.maps.places.SearchBox(searchElement);
@@ -7,8 +6,7 @@ const searchBox = new google.maps.places.SearchBox(searchElement);
 // Use the google specific listener (not an eventListener!)
 // When the user selects a place, 
 searchBox.addListener('places_changed', () => {
-
-  console.log('searchBox.getPlaces():', searchBox.getPlaces());
+  // console.log('searchBox.getPlaces():', searchBox.getPlaces());
 
   // Get the place object (it's returned in an array)
   // getPlaces() must do a kind of request to the maps.googleapis.com API for the object
@@ -45,10 +43,20 @@ const windElement = document.querySelector('[data-wind]');
 const precipitationElement = document.querySelector('[data-precipitation]');
 const temperatureElement = document.querySelector('[data-temperature]');
 
+const icon = new Skycons({ color: '#222' });
+// Set default displayed icon (before place selected)
+icon.set('icon', 'clear-day');
+icon.play();
+
 const setWeatherData = (data, place) => {
   locationElement.textContent = place;
   statusElement.textContent = data.summary;
   temperatureElement.textContent = data.temperature;
   precipitationElement.textContent = `${data.precipProbability * 100}`;
   windElement.textContent = data.windSpeed;
+  // First parm is the id of the canvas HTML element. 
+  // Second param is the name of the icon (it's a property of the returned data object)
+  icon.set('icon', data.icon);
+  // Start animation!
+  icon.play();
 }
